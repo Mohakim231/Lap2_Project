5f2c69f0bff2ba3b49d4e1374af5ecd7e594ca3e
Mohakim231 <mohamedhakim0381@gmail.com>
2023-02-13 17:22:39 +0000

    Adding auth

--- api/routers/events.js
+++ api/routers/events.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const eventsController = require('../controllers/events');
+const authenticator = require('../middleware/authenticator');
 
 const eventsRouter = Router();
 
-eventsRouter.get("/", eventsController.index);
+eventsRouter.get("/", authenticator, eventsController.index);
 eventsRouter.get("/:id", eventsController.show);
 eventsRouter.post("/", eventsController.create);
 eventsRouter.patch("/:id", eventsController.update);
